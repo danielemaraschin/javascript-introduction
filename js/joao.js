cd31c997d4fc98ad6fa47c3190627ce4c4e6c3bd
Daniele <danikmaraschin@gmail.com>
2020-02-26 20:47:57 +0100

    Working with querySelector, for iteration, or operation, AND operation, classList.add and toFixed

---
+++ js/joao.js
@@ -0,0 +1,6 @@
+var produtos = document.querySelector("lista-produtos");
+
+for( var i=0 ; i < produtos.length ; i++){
+    var produtos  = produto[i];
+    var nomeDoProduto = produto.textContent;
+    console.log(nomeDoProduto);
--- js/principal.js
+++ js/principal.js
@@ -1,15 +1,38 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent= "Aparecida Nutricionista";
 
+var pacientes = document.querySelectorAll(".paciente");
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
 
-var peso = tdPeso.textContent;
+for (var i = 0; i < pacientes.length; i++){
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var paciente = pacientes [i];
 
-console.log(paciente);
-console.log(tdAltura);
-console.log(altura); 
+    var tdPeso = paciente.querySelector(".info-peso");
+        var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+        var altura = tdAltura.textContent;
+    
+    var tdImc= paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 1000){
+        pesoEhValido = false;
+        tdImc.textContent = "Peso Inválido";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (altura <= 0 || altura >= 3.00){
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (alturaEhValida && pesoEhValido){
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+}
